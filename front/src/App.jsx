152a1ff60cb22a0d721d1f557ff4bfe1e8c48d21
V-U-Simon <V-U-Simon@yandex.ru>
2023-09-08 11:23:42 +0300

    front: router component

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef, useContext } from "react";
 import axios from "axios";
 
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
@@ -8,88 +8,35 @@ import ToDoList from "./components/Todo";
 import UserList from "./components/UserList";
 import NotFound from "./components/NotFound";
 import LoginForm from "./components/LoginForm";
+import Logout from "./components/Logout";
+import { AuthContext } from "./components/AuthContext";
+import useProjectActions from "./components/useProjectActions";
+import useTodoActions from "./components/useTodoActions";
+import useUserActions from "./components/useUserActions";
 
 const App = () => {
+  const [token] = useContext(AuthContext);
   const [state, setState] = useState({
     users: [],
     projects: [],
     todos: [],
-    token: "",
   });
 
+  const projectActions = useProjectActions(state, setState);
+  const todoActions = useTodoActions(state, setState);
+  const userActions = useUserActions(state, setState);
+
+  const isAuth = () => Boolean(token);
+
   useEffect(() => {
-    const token = localStorage.getItem("token");
-    console.log("token: " + token);
-    setState((prevState) => ({ ...prevState, token }));
-    getData();
-  }, []);
-
-  const obtainAuthToken = (login, password) => {
-    axios
-      .post("http://127.0.0.1:8000/api-auth-token/", {
-        username: login,
-        password: password,
-      })
-      .then((response) => {
-        const token = response.data.token;
-        localStorage.setItem("token", token);
-        setState((prevState) => ({ ...prevState, token }));
-        getData();
-      })
-      .catch((error) => console.log(error));
-  };
-
-  const logOut = () => {
-    localStorage.setItem("token", "");
-    setState((prevState) => ({ ...prevState, token: "" }));
-    getData();
-  };
-
-  const isAuth = () => localStorage.getItem("token");
-
-  const getHeaders = () => {
-    if (isAuth()) {
-      return {
-        Authorization: `Token ${localStorage.getItem("token")}`,
-        "Content-Type": "application/json",
-      };
+    console.log("state: " + token);
+    console.log("storage: " + localStorage.getItem("token"));
+    if (token) {
+      projectActions.list();
+      todoActions.list();
+      userActions.list();
     }
-    return {};
-  };
-
-  const getData = () => {
-    const headers = getHeaders();
-    console.log("Headers:", headers);
-    axios
-      .get("http://127.0.0.1:8000/api/users/", { headers })
-      .then((response) => {
-        setState((prevState) => ({ ...prevState, users: response.data }));
-      })
-      .catch((error) => {
-        setState((prevState) => ({ ...prevState, users: [] }));
-        console.log(error);
-      });
-
-    axios
-      .get("http://127.0.0.1:8000/api/projects/", { headers })
-      .then((response) => {
-        setState((prevState) => ({ ...prevState, projects: response.data }));
-      })
-      .catch((error) => {
-        setState((prevState) => ({ ...prevState, projects: [] }));
-        console.log(error);
-      });
-
-    axios
-      .get("http://127.0.0.1:8000/api/todo/", { headers })
-      .then((response) => {
-        setState((prevState) => ({ ...prevState, todos: response.data }));
-      })
-      .catch((error) => {
-        setState((prevState) => ({ ...prevState, todos: [] }));
-        console.log(error);
-      });
-  };
+  }, [token]);
 
   return (
     <div>
@@ -104,13 +51,7 @@ const App = () => {
           <li>
             <Link to="/todos">Задачи</Link>
           </li>
-          <li>
-            {isAuth() ? (
-              <button onClick={logOut}>Logout</button>
-            ) : (
-              <Link to="/login">Login</Link>
-            )}
-          </li>
+          <li>{isAuth() ? <Logout /> : <Link to="/login">Login</Link>}</li>
         </nav>
 
         <Routes>
@@ -123,18 +64,26 @@ const App = () => {
           <Route
             exact
             path="/projects"
-            element={<ProjectList projects={state.projects} />}
+            element={
+              <ProjectList
+                projects={state.projects}
+                projectActions={projectActions}
+              />
+            }
           />
           <Route
             exact
             path="/todos"
-            element={<ToDoList todos={state.todos} />}
+            element={
+              <ToDoList
+                todos={state.todos}
+                todoActions={todoActions}
+                projects={state.projects}
+                users={state.users}
+              />
+            }
           />
-          <Route
-            exact
-            path="/login"
-            element={<LoginForm obtainAuthToken={obtainAuthToken} />}
-          />
+          <Route exact path="/login" element={<LoginForm />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
