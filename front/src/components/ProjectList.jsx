1f1e6f184fa80a8a4a6826403ba8d6cf07a596e1
V-U-Simon <V-U-Simon@yandex.ru>
2023-09-08 11:22:23 +0300

    front: project components

--- front/src/components/ProjectList.jsx
+++ front/src/components/ProjectList.jsx
@@ -1,34 +1,52 @@
-export default function ProjectList({ projects }) {
+import ProjectsForm from "./ProjectForm";
+
+export default function ProjectList({ projects, projectActions }) {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Название</th>
-          <th>Создатель</th>
-          <th>Ссылка</th>
-          <th>Создано</th>
-          <th>Обновлено</th>
-          <th>Активно</th>
-        </tr>
-      </thead>
-      <tbody>
-        {projects.map((project) => (
-          <ProjectItem key={project.id} project={project} />
-        ))}
-      </tbody>
-    </table>
+    <>
+      <ProjectsForm props={projects} createProject={projectActions.create} />
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Название</th>
+            <th>Ссылка</th>
+            <th>Создано</th>
+            <th>Обновлено</th>
+            <th>Активно</th>
+          </tr>
+        </thead>
+        <tbody>
+          {projects.map((project) => (
+            <ProjectItem
+              key={project.id}
+              project={project}
+              removeProject={projectActions.remove}
+            />
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
-function ProjectItem({ project }) {
+function ProjectItem({ project, removeProject }) {
+  console.dir(project);
   return (
     <tr>
+      <td>{project.id}</td>
       <td>{project.name}</td>
-      <td>{project.creator}</td>
-      <td>{project.repository_url}</td>
+      <td>{project.repositoryUrl}</td>
       <td>{project.created}</td>
       <td>{project.updated}</td>
-      <td>{project.is_active.toString()}</td>
+      <td>{project.isActive.toString()}</td>
+      <td>
+        <button
+          className="brn text-red-600"
+          onClick={(event) => removeProject(project.id)}
+        >
+          delete
+        </button>
+      </td>
     </tr>
   );
 }
